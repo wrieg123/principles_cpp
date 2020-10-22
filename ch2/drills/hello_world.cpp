f910fb7a4807d542e2e0d4642aa482447c5aa6b9
wrieg123 <will.r.rieger@gmail.com>
2020-10-22 09:53:58 -0500

    Hello, World!

---
+++ ch2/drills/hello_world.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+
+int main() {
+
+  cout << "Hello, World!" << endl;
+
+  return 0;
+}
